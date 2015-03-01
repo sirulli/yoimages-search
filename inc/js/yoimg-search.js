456d8c066d010e341f745bcc4e6da02ac02fe05c
matteo.cajani <matteo.cajani@gmail.com>
2015-03-01 14:47:44 +0100

    added search tab to browse router

--- inc/js/yoimg-search.js
+++ inc/js/yoimg-search.js
@@ -2,9 +2,27 @@ jQuery(document).ready(function() {
 	if (wp && wp.media && wp.media.view && wp.media.view.MediaFrame && wp.media.view.MediaFrame.Select) {
 		window.originalWpMedia = wp.media;
 		wp.media.view.MediaFrame.SelectWithYoimgSearch = wp.media.view.MediaFrame.Select.extend({
+			bindHandlers : function() {
+				this.on('content:render:yosearch', this.yoimgSearch, this);
+				wp.media.view.MediaFrame.Select.prototype.bindHandlers.call(this);
+			},
 			browseRouter : function(routerView) {
-				console.log('wheee we are using SelectWithYoimgSearch!');
-				return wp.media.view.MediaFrame.Select.prototype.browseRouter.call(this, routerView);
+				wp.media.view.MediaFrame.Select.prototype.browseRouter.call(this, routerView);
+				routerView.set({
+					yosearch : {
+						text : 'Search', // TODO l10n.searchImagesTitle
+						priority : 9
+					}
+				});
+			},
+			yoimgSearch : function() {
+				if (console) {
+					console.log('wheee we are ready to search with SelectWithYoimgSearch!');
+				}
+				// TODO tmp
+				this.content.set(new wp.media.view.UploaderInline({
+					controller : this
+				}));
 			}
 		});
 		var mediaWithYoimgSearch = function(attributes) {
